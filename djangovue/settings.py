29505f35b6562cb891fa712bed6ea041a7c8e668
Brennan <flood.brennan@gmail.com>
2020-04-25 17:28:53 -0400

    add cors middleware

--- djangovue/settings.py
+++ djangovue/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
 ]
 
 ROOT_URLCONF = 'djangovue.urls'
