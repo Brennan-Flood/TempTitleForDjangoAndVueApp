d3041a310dc1e5ade169c320aa186ba9385a847c
Brennan <flood.brennan@gmail.com>
2020-04-25 17:32:33 -0400

    add temp front page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,117 @@
+import auth0 from 'auth0-js'
+import EventEmitter from 'eventemitter3'
+import router from './../router'
+
+export default class AuthService {
+  authenticated = this.isAuthenticated()
+  authNotifier = new EventEmitter()
+
+  constructor () {
+    this.login = this.login.bind(this)
+    this.setSession = this.setSession.bind(this)
+    this.logout = this.logout.bind(this)
+    this.isAuthenticated = this.isAuthenticated.bind(this)
+    this.handleAuthentication = this.handleAuthentication.bind(this)
+  }
+
+  // create an instance of auth0.WebAuth with your
+  // API and Client credentials
+  auth0 = new auth0.WebAuth({
+    domain: '<YOUR_AUTH0_DOMAIN>',
+    clientID: '<YOUR_CLIENT_ID>',
+    redirectUri: '<YOUR_CALLBACK_URL>',
+    audience: '<YOUR_AUDIENCE>',
+    responseType: 'token id_token',
+    scope: 'openid profile'
+  })
+
+  // this method calls the authorize() method
+  // which triggers the Auth0 login page
+  login () {
+    this.auth0.authorize()
+  }
+
+  // this method calls the parseHash() method of Auth0
+  // to get authentication information from the callback URL
+  handleAuthentication () {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult)
+      } else if (err) {
+        console.log(err)
+        alert(`Error: ${err.error}. Check the console for further details.`)
+      }
+      router.replace('/')
+    })
+  }
+
+  // stores the user's access_token, id_token, and a time at
+  // which the access_token will expire in the local storage
+  setSession (authResult) {
+    this.accessToken = authResult.accessToken
+    this.idToken = authResult.idToken
+    this.profile = authResult.idTokenPayload
+    this.expiresAt = authResult.expiresIn * 1000 + new Date().getTime()
+    this.authNotifier.emit('authChange', {authenticated: true})
+  }
+
+  // remove the access and ID tokens from the
+  // local storage and emits the authChange event
+  logout () {
+    delete this.accessToken
+    delete this.idToken
+    delete this.expiresAt
+    this.authNotifier.emit('authChange', false)
+    // navigate to the home route
+    router.replace('/')
+  }
+
+  // checks if the user is authenticated
+  isAuthenticated () {
+    // Check whether the current time is past the
+    // access token's expiry time
+    return new Date().getTime() < this.expiresAt
+  }
+
+  // a static method to get the access token
+  getAuthToken () {
+    return this.accessToken
+  }
+
+  // a method to get the User profile
+  getUserProfile (cb) {
+    return this.profile
+  }
+}
+You will need to replace <YOUR_AUTH0_DOMAIN>, <YOUR_CLIENT_ID>, <YOUR_CALLBACK_URL>, and <YOUR_AUDIENCE> with the values from your client and API settings. The audience property refers to the identifier of your Auth0 API (i.e. if you followed the instructions, it will be https://django-vuejs-api).
+
+Creating the Homepage
+Now, you are going to refactor your Vue.js homepage to allow users to authenticate via Auth0 and to fetch public and private messages. To do that, open ./frontend/src/App.vue and replace the <template> code with this:
+
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="!authenticated"
+      @click="login()">
+      Log In
+    </button>
+
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="authenticated"
+      @click="privateMessage()">
+      Call Private
+    </button>
+
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="authenticated"
+      @click="logout()">
+      Log Out
+    </button>
+    {{ message }}
+    <br>
   </div>
 </template>
 
